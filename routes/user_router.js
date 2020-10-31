b82e5b352c72eef6f885ccd292d6d9b3cf4b8f4e
prats7 <pratyushvishal04@gmail.com>
2020-11-01 00:07:22 +0530

    Add router for upgrade

--- routes/user_router.js
+++ routes/user_router.js
@@ -1,6 +1,8 @@
-const { createUser } = require("../controllers/api/user_controller");
+const { createUser, upgradeService } = require("../controllers/api/user_controller");
 const router = require("express").Router();
 
 router.post("/",createUser);
+//For upgrade
+router.patch("/",upgradeService);
 
 module.exports = router;
